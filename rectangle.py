a80d078bab898af3fe280be53bd901169a63466c
mesrumpled <marchsaper@gmail.com>
2016-03-08 23:45:32 -0500

    Now with a dynamic year and titles

--- rectangle.py
+++ rectangle.py
@@ -1,81 +1,15 @@
-""" labelDemo.py
-    creating a basic label sprite"""
-    
-import pygame
-pygame.init()
+""" Visualizes input data over the years 1990-2014 as an interactive bar graph.
 
-# screen = pygame.display.set_mode((640, 480))    
-    
-class Label(pygame.sprite.Sprite):
-    """ Label Class  
-        Attributes:
-            font: any pygame Font or SysFont objects
-            text: text to display
-            center: desired position of label center (x, y)
-    """
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.font = pygame.font.SysFont("None", 30)
-        self.text = ""
-        self.center = (320, 240)
-                
-    def update(self):
-        self.image = self.font.render(self.text, 1, (0, 0, 0))
-        self.rect = self.image.get_rect()
-        self.rect.center = self.center
-        
-# def main():
-#     pygame.display.set_caption("Label demo")
-    
-#     background = pygame.Surface(screen.get_size())
-#     background.fill((255, 255, 255))
-#     screen.blit(background, (0, 0))
-    
-#     label1 = Label()
-#     label2 = Label()
-#     labelEvent = Label()
-#     allSprites = pygame.sprite.Group(label1, label2, labelEvent)
-    
-#     label1.text = "Hi. I'm a label."
-#     label1.center = (100, 100)
-    
-#     label2.text = "I'm another label."
-#     label2.center = (400, 400)
-    
-#     clock = pygame.time.Clock()
-#     keepGoing = True
-#     while keepGoing:
-#         clock.tick(30)
-        
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 keepGoing = False
-#             elif event.type == pygame.MOUSEMOTION:
-#                 (mouseX, mouseY) = pygame.mouse.get_pos()
-#                 labelEvent.text = "mouse: (%d, %d)" % (mouseX, mouseY)
-#             elif event.type == pygame.MOUSEBUTTONDOWN:
-#                 labelEvent.text = "button press"
-#             elif event.type == pygame.KEYDOWN:
-#                 labelEvent.text = "key down"
-        
-#         allSprites.clear(screen, background)
-#         allSprites.update()
-#         allSprites.draw(screen)
-        
-#         pygame.display.flip()
+@authors: March Saper, Emma Price
 
-
-
-
-
+Software Design 2016 Olin College of Engineering"""
+    
 import pygame
 from pygame.locals import QUIT, KEYDOWN, MOUSEMOTION
 import time
 import csv
 import random
-
-pygame.init()
-
+   
 
 def generate_rates(f):
     """Generates a dictionary containing the rates for each element in the data set"""
@@ -104,8 +38,6 @@ SIZE = (1700, 1040)
 
 STATE_COLORS = generate_colors(STATE_RATES)
 
-print STATE_RATES.keys()[0]
-print STATE_COLORS.keys()[0]
 
 
 
@@ -119,8 +51,12 @@ class View(object):
         self.screen.fill(pygame.Color('white'))
         for state in self.model.states_rect:
             pygame.draw.rect(self.screen, pygame.Color(*STATE_COLORS[state]), self.model.states_rect[state])
-        self.screen.blit(model.title, (SIZE[0]/2.0,SIZE[1]/9.0))
-        self.screen.blit(model.astate, (200, 200))
+    
+        self.screen.blit(model.title, (SIZE[0]/17,SIZE[1]/17))
+        self.screen.blit(model.year, (SIZE[0]/17,SIZE[1]/10))
+
+        for i, state in enumerate(model.states_names):
+            self.screen.blit(model.states_names[state], (i*(SIZE[0]/50.0), 615))
         pygame.display.update()
 
 
@@ -129,16 +65,28 @@ class Model(object):
     def __init__(self):
         self.states_rect = {}
         for i, state in enumerate(STATE_RATES):
-            if i < 1:
-                print state
-            self.states_rect[state] = pygame.Rect(i*(SIZE[0]/50.0) + 10, 600, (SIZE[0]/50.0-20), 0)
+            self.states_rect[state] = pygame.Rect(i*(SIZE[0]/50.0) + 5, 600, (SIZE[0]/50.0-10), 0)
 
-        freetypeT = pygame.font.SysFont("serif", 72)
-        self.title = freetypeT.render("Hi", True, (0, 128, 0))
 
-        freetype = pygame.font.SysFont("serif", 24)
-        self.astate = freetype.render(STATE_RATES.keys()[0], True, STATE_COLORS.values()[0])
-        self.astate = pygame.transform.rotate(self.astate, 90)
+
+        # initialize the title
+        freetypeT = pygame.font.SysFont("serif", 54)
+        self.title = freetypeT.render("Hi", True, (0, 0, 0))
+
+        # initialize the year
+        freetypeY = pygame.font.SysFont("serif", 38)
+        self.year = freetypeY.render("year", True, (0, 0, 0))
+
+
+        # initialize the state names
+        self.states_names = {}
+        for i, state in enumerate(STATE_RATES):
+            freetype = pygame.font.SysFont("serif", 24)
+            self.states_names[state] = freetype.render(STATE_RATES.keys()[i], True, (0, 0, 0))
+            self.states_names[state] = pygame.transform.rotate(self.states_names[state], 90)
+            print STATE_COLORS[STATE_RATES.keys()[i]]
+            print STATE_COLORS[state]
+
         
     def update(self, position):
         # Update all model attributes based on the position of the mouse
@@ -146,7 +94,11 @@ class Model(object):
                 scaled_value = (float(position[0])/SIZE[0])*len(STATE_RATES[state])
                 chosen_index = int(scaled_value)
                 self.states_rect[state].height = (-5*float(STATE_RATES[state][chosen_index]))
-        
+
+
+        freetypeY = pygame.font.SysFont("serif", 38)
+        p = str(1990 + chosen_index)
+        self.year = freetypeY.render(p, True, (0, 0, 0))
 
         
 class MouseController(object):
@@ -180,5 +132,3 @@ if __name__ == '__main__':
         view.draw()
         time.sleep(.001)
     
-
-
